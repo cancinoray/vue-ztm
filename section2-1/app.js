b776e8927afff0c0bf2e022d261c632bd9c873a5
cancinoray <cancinoray@gmail.com>
2022-07-01 20:58:29 +0800

    Updating all files

--- section2-1/app.js
+++ section2-1/app.js
@@ -1,1 +1,37 @@
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({ //store in the variable
+    data(){
+        return {
+            firstName: '',
+            lastName: ''
+        }
+    },
+    methods: {
+        fullName(){
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
+        }
+    }
+}).mount('#app')
+
+//We Change the firstName after a 2 seconds.
+// setTimeout(() => {
+//     vm.firstName = 'Bob' //it should be vm.data.firstName, but vue make it short to access the data.
+// }, 2000)
+
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             firstName: 'Ray',
+//             lastName: 'Cancino'
+//         }
+//     }
+// }).mount('#app2')
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             count: 0
+//         }
+//     }
+// }).mount('#app3')
+
